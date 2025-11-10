df323efaffd13b46bb819001f6ebeda4a6b0ef26
cvm1912 <shivam.fcse@gmail.com>
2025-11-10 14:59:08 +0530

    feat(task-scheduling): added time scheduling event

---
+++ index.js
@@ -0,0 +1,11 @@
+import schedule from 'node-schedule';
+
+schedule.scheduleJob('*/30 * * * * *', () => {
+  console.log('👋 Hello Scheduler! Time:', new Date().toLocaleTimeString());
+});
+
+
+const date = new Date(Date.now() + 60000);
+schedule.scheduleJob(date, () => {
+    console.log("This runs 1 minute from now", date.toLocaleTimeString());
+});
